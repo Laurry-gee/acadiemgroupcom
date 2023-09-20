8912adc802f4a43f2ee609f67b54704864d66b1e
Gregory Lamar <laurysevertson@icloud.com>
2023-09-20 22:06:55 +0100

    Create edgemesh-sw.js
    Signed-off-by: Gregory Lamar  <laurysevertson@icloud.com>

---
+++ edgemesh-sw.js
@@ -0,0 +1,13 @@
+
+<!-- Edgemesh Client Begin -->
+<script type="application/javascript">
+  EDGEMESH = {
+    lite: true
+  }
+</script>
+<script
+  async
+  type="application/javascript"
+  src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
+</script>
+<!-- Edgemesh Client End -->->
