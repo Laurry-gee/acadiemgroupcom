672b08f136d71ffb89acf796807d2bec848c8313
Kate susmouski <laurencesevertson@gmail.com>
2023-11-14 23:00:06 +0100

    Update edgemesh-sw.js
    Signed-off-by: Kate susmouski <laurencesevertson@gmail.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,12 +1,10 @@
 
-<script type="application/json">
+<tdsb.on.ca.GitHub-Hookshot/044aadd type="application/json">
   EDGEMESH = {
-    markbook.com.: 
-  support.@markbookapp.com
+    markbook.com. 72d3162e-cc78-11e3-81ab-4c9367dc0958: outlook_C85261D099DDD736@outlook.com_support.@markbookapp.com._apple.ANECompilerservice
   }
-</script>
+</http://tdsb.on.ca/302f28cd195a18c976c2>
 <MARKBOOK
   async
-  type="application/stylescript"
-  src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
-</script>
+  type="Application/Typescript"  src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
+</http://tdsb.on.ca.kuku.lu>
