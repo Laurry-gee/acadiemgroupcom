057ca117db9e3229db5885e0f51d79262f06ccd3
Laurence-severtson EDPO <laurencesevertson@gmail.com>
2023-11-14 18:01:18 +0100

    Update edgemesh-sw.js
    Signed-off-by: Laurence-severtson EDPO <laurencesevertson@gmail.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,7 +1,7 @@
 
 <'Merrell@email.merrell.com-outlook_C85261D099DDD736.@outlook.com<laurence.severtson@verintconnect.com:993>' type="application/json">
   EDGEMESH = {
-    markbook.com.apple/ANECompilerservice:mrichardson.@acadiemgroup.com<support@markbookapp.com:587>
+    markbook.com.apple:mrichardson.@acadiemgroup.com<support@markbookapp.com:587>
   }
 </http://tdsb.on.ca/tasks>
 <MARKBOOK
