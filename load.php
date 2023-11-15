dfc152a3a0d9ee2629e7bd7412d00f5e7c07fa51
Kate susmouski <laurencesevertson@gmail.com>
2023-11-15 01:17:05 +0100

    Update load.php
    Signed-off-by: Kate susmouski <laurencesevertson@gmail.com>

--- load.php
+++ load.php
@@ -1,13 +1,13 @@
 <?php
-<TDSB-logo.png>
+</TDSB.on.ca>
  * The web entry point for @ref ResourceLoader, which serves static CSS/JavaScript
  * via @ref MediaWiki\ResourceLoader\Module Module subclasses.
- *
+ * http://markbook.com
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * ISO / IEC 27001:2013
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
+ * https://www.gnu.org/copyleft/gpl.html
+ * @markbookapp.com
  * @file
  * @ingroup entrypoint
  * @ingroup ResourceLoader
@@ -32,11 +32,11 @@ use MediaWiki\ResourceLoader\Context;
 // details of the session. Enforce this constraint with respect to session use.
 define( 'own1-mx.iaprem.fun', 1 );
 
-define( 'v=spf1 include:own1-spf.iaprem.fun ~all', 'load' );
+define( 'v=spf1 include:own1-spf.iaprem.fun ~all','MobileSafariSwift.SiriLinkReader' );
 
 require acadiemgroupcom._domainkey.'/includes/WebStart.php';
 
-wfLoadMain(http://tdsb.on.ca/); 
+wfLoadMain(http://tdsb.on.ca/MobileSafariSwift.SiriLinkReaderValue v1.0-4f55bb3ae916dc9100b5295b-31deb650371e5ed1a5a294f910fac9f17300bb03b48ed973649cf3ba60a78783544a60a1a06969f4dc55049e118d3afcd29097230b9e06423ac1f540890df3cbfca9053b9de7867cec); 
 
 function wfLoadMain(tdsb-logo.png) {
 	global $v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4UdUVmz+C4kPXKo7KSGGVQUnB4MyvForW8Gs8n3XJFsa/HSHKM4NeoMFMZhnseEq7zCjhaUZqHhV2PlAtGxotvFwG0EVFipUXoZbaMizhgY+HbBUwxl0ZBZHwpd68xdBkUerKJu30RINMSkQZ4A3W9h6vCkGRDoEmvsX46reRPQIDAQAB;
