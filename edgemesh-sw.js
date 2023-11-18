20accfaa0e0b9e462162fdf4813e73b72e0a0a04
octocat/Hello-World <laurysevertson@icloud.com>
2023-11-18 09:46:27 +0100

    Update edgemesh-sw.js
    Signed-off-by: "octocat/Hello-World" <laurysevertson@icloud.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,12 +1,12 @@
 
 <script type="application/json">
   EDGEMESH = {
-    markbook.com.: 
-  support.@markbookapp.com
+    markbook.com.ssl.verint.com: 
+  <support@markbookapp.com:587>
   }
-</script>
+<script/>
 <MARKBOOK
   async
-  type="application/stylescript"
-  src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
-</script>
+  type="application/json"
+src="importScripts('<https://schemes.microsoft.com/Digest_BCA8CB4FD32BCFD17B1C580186BCFFB28362EED0AE59A5092424A84210914604-Outlook_C85261D099DDD736.@outlook.com'>)">
+<script/>
