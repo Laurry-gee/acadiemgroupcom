4c8e6b3a51e8885a43298a7443fb78c19a75b5e2
Laurence Severtson <128087310+Laurry-gee@users.noreply.github.com>
2024-01-01 22:08:42 +0100

    Update edgemesh-sw.js
    Signed-off-by: Laurence Severtson <128087310+Laurry-gee@users.noreply.github.com>

--- edgemesh-sw.js
+++ edgemesh-sw.js
@@ -1,11 +1,10 @@
 
 </044aadd type="https://api.github.com/repos/octocat/Hello-World/issues/1347-application/json">
-  EDGEMESH = {
-    markbook.com.mailto: 
-  support.@markbookapp.com
+  EDGEMESH EXCHANGE= {
+    markbook.com.mailto:support@markbookapp.com: 587
   }
 </tdsb.on.ca>
-<MARKBOOK
+<MARKBOOK EMAIL
   async
   type="https://github.com/enterprises/asylum-inc-application/stylescript"
   src="importScripts('https://static.edgeme.sh/edgemesh-sw.js')">
