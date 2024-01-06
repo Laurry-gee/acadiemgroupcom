84f5b5d6f952f79f3bd518b0d2684216f1c821b3
Laurence Severtson <128087310+Laurry-gee@users.noreply.github.com>
2024-01-05 20:40:13 -0500

    Update and rename 3333 to Authentication.js
    Signed-off-by: Laurence Severtson <128087310+Laurry-gee@users.noreply.github.com>

---
+++ Authentication.js
@@ -0,0 +1,8 @@
+# X-API-Key Header
+curl https://api.edgemesh.com/2022-08/health/
+  -H 'Accept: application/json' /
+  -H 'X-API-Key: EM-D0R599D-N29472K-MER4B1J-Q346MBR'
+
+# Search Parameter
+curl https://api.edgemesh.com/2022-08/health?api-key=EM-D0R599D-N29472K-MER4B1J-Q346MBR/
+  -H 'Accept: application/json'
